d6971de123f50b19979f6799c6d73cedda93bf01
Nithesh, Kandregula <nithesh.kandregula@principal.com>
2022-03-09 12:42:56 +0530

    Java Project with JDK1.4 has been created Mr.A

---
+++ Task/src/Task/Demo.java
@@ -0,0 +1,14 @@
+package Task;
+
+import java.util.*;
+public class Demo {
+
+	public static void main(String[] args) {
+		List l= new ArrayList();
+		l.add(100);
+		l.add("Hi");
+		l.add(3.4);
+		System.out.println(l);
+	}
+
+}
